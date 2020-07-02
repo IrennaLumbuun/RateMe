efc3a967ccc308310dac0b640fdc15cb4d43e0fd
Irenna Lumbuun <irennanicole1@gmail.com>
2020-07-02 13:50:59 -0700

    add face detection to scraper

--- face.py
+++ face.py
@@ -1,35 +1,43 @@
 import face_recognition
 from PIL import Image, ImageDraw
+import urllib.request
+import ssl
 
-image_path = '../../Downloads/photo.jpg'
-image = face_recognition.load_image_file(image_path)
 
-# get arrays of locations of each faces
-# [top, right, bottom, left]
-face_location = face_recognition.face_location(image)
+'''
+    Load image from url & save it
+'''
 
-print(face_location)
+context = ssl._create_unverified_context()
+url = 'https://preview.redd.it/0jzy2kgbhb851.jpg?width=640&crop=smart&auto=webp&s=3d9e423ba3cf5bb8c2c842b52245c63c8ff29901'
 
-# image matching
+def get_face(url: str) -> list:
+    image = Image.open(urllib.request.urlopen(url, context=context))
+    # TODO: save this in cloud database
+    image_path = 'test.jpg'
+    image.save(image_path)
 
-# encode image
-known_image = face_recognition.face_encodings(image)[0]
-# result = face_recognition.compare_faces([known_image], unknown_image)
-# true if same,false otherwise
+    # get face location
+    image = face_recognition.load_image_file(image_path)
+    face_location = face_recognition.face_locations(image)
+    print(face_location)
 
-# poll faces
-for face in face_location:
-    top, right, bottom, left = face
+    '''
+        Poll face from image & save it
+    '''
+    top, right, bottom, left = face_location[0]
     face_image = image[top:bottom, left:right]
+    polled_image = Image.fromarray(face_image)
 
-    pil_image = Image.fromarray(face_image)
-    # pil_image.show()
-    # pil_image.save(f'{top}.jpg')
+    # TODO: save this in cloud database
+    polled_image_path = 'polled_test.jpg'
+    polled_image.save(polled_image_path)
 
-#draw on faces
-face_encodings  = face_recognition.face_encodings(image, face_location)
-# convert to pil
-pil = Image.fromarray(image)
-draw = ImageDraw.Draw(pil)
+    polled_image = face_recognition.load_image_file(polled_image_path)
+    face_encodings  = face_recognition.face_encodings(polled_image)
+    
+    return face_encodings
 
 #see more https://www.youtube.com/watch?v=QSTnwsZj2yc
+# or https://medium.com/better-programming/step-by-step-face-recognition-in-images-ad0ad302058a
+
--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 # parse HTML, put it in a json file
 
 import os
+import re
 import json 
 import requests # to sent GET requests
 from bs4 import BeautifulSoup # to parse HTML
@@ -10,6 +11,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from time import sleep
+from face import get_face
 
 url = 'https://www.reddit.com/r/truerateme/'
 
@@ -49,42 +51,53 @@ class Crawler():
             # TODO: continue saving all images
             for i in images:
                 src = i.get_attribute('src')
-                print(src)
+                encoding = get_face(src)
+                print(encoding)
             
-            # if there is "view entire discussion" button, click on it
             comments = self.driver.find_elements_by_class_name('_1qeIAgB0cPwnLhDF9XSiJM')
-            total_score = 0
 
             if comments == []:
                 self.close_window()
                 continue
-
-            comments_with_score = 0
-            for com in comments:
-                text = com.get_attribute("innerHTML")
-                # this comment keeps showing up for some reason
-                # idk where it's from, so I have to manually skip it
-                if len(text) > 80:
-                    continue
-                if '<a' in text:
-                    continue
-                if 'rule' in text.lower():
-                    continue
-                for i in range(0, len(text)):
-                    if text[i].isdigit():
-                        print(text)
-                        comments_with_score += 1
-                        # check if there's a '.' (that's not at the end of sentence) to indicate floating points
-                        if len(text) > i + 1 and text[i + 1] == '.':
-                            score = float(text[i:i+3])
-                        else:
-                            score = float(text[i])
-                        total_score += score
-                        break
-            if comments_with_score > 0:
-                print(f'count: {comments_with_score}')
-                print(f'average = {total_score / comments_with_score}')
+            self.analyse_comments(comments)
             self.close_window()
+
+
+    def analyse_comments(self, comments: list):
+        total_score = 0
+        comments_with_score = 0
+
+        for com in comments:
+            text = com.get_attribute("textContent")
+            # very long comments normally not a rating but
+            # more of an advice
+            if len(text) > 80:
+                continue
+            # skip links to simplify the code
+            if 'http' in text.lower():
+                print('http')
+                continue
+            if 'rule' in text.lower() or 'guide' in text.lower():
+                print('rule')
+                continue
+
+            dec_list = re.findall('\d+\.?\d*', text)
+            if len(dec_list) == 0:
+                continue
+
+            d = 0
+            for d in dec_list:
+                # find the first instance of d that is between 0 and 10
+                d = float(d)
+                if d > 0 and d <= 10:
+                    comments_with_score += 1
+                    total_score += d
+                    break
+            
+        if comments_with_score > 0:
+            print(f'count: {comments_with_score}')
+            print(f'average = {total_score / comments_with_score}')
+
 
     def close_window(self):
         try:
@@ -99,6 +112,7 @@ class Crawler():
 crawler = Crawler()
 crawler.driver.get('https://www.reddit.com/r/truerateme/')
 
+''' uncomment when not on testing mode
 for loop in range(0, 50):
     # wait for it to load
     print('scrolling...')
@@ -107,4 +121,6 @@ for loop in range(0, 50):
 
 #go back to the top of the page
 crawler.driver.execute_script("window.scrollTo(0, 0);")
+'''
+
 crawler.get_data()
